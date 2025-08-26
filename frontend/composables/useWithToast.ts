cc542a87aad367cf1d839b40e7c1388a925db85b
Patrick Roza <contact@patrickroza.com>
2025-08-26 16:41:54 +0200

    useWithToast idea..

---
+++ frontend/composables/useWithToast.ts
@@ -0,0 +1,42 @@
+import { Cause, Effect, type Option } from "effect-app"
+
+type ToastOptions<A, E, Args extends ReadonlyArray<unknown>> = {
+  onWaiting: string | ((...args: Args) => string)
+  onSuccess: string | ((a: A, ...args: Args) => string)
+  onFailure: string | ((error: Option.Option<E>, ...args: Args) => string)
+}
+
+export const useWithToast = () => {
+  const toast = useToast()
+  return <A, E, Args extends ReadonlyArray<unknown>, R>(
+    options: ToastOptions<A, E, Args>,
+  ) =>
+    Effect.fnUntraced(function* (self: Effect.Effect<A, E, R>, ...args: Args) {
+      const toastId = toast.info(
+        // .loading
+        typeof options.onWaiting === "string"
+          ? options.onWaiting
+          : options.onWaiting(...args),
+      )
+      return yield* self.pipe(
+        Effect.tap(a => {
+          toast.success(
+            typeof options.onSuccess === "string"
+              ? options.onSuccess
+              : options.onSuccess(a, ...args),
+            { id: toastId },
+          )
+        }),
+        Effect.tapErrorCause(cause =>
+          Effect.sync(() => {
+            toast.error(
+              typeof options.onFailure === "string"
+                ? options.onFailure
+                : options.onFailure(Cause.failureOption(cause), ...args),
+              { id: toastId },
+            )
+          }),
+        ),
+      )
+    })
+}
